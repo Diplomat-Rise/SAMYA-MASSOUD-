1f8235e64a404f75dec5b39a0f705e05497c9e69
Diplomat-Rise <diploriseit@gmail.com>
2025-06-23 11:32:19 +0400

    Update send-contact-details.js

--- JS/send-contact-details.js
+++ JS/send-contact-details.js
@@ -56,7 +56,7 @@ function createContactPopup() {
       `Hi, here are my contact details:\n\nName: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${userMessage}`
     );
 
-    const recipientNumber = "+971554297433"; // Replace with your WhatsApp number
+    const recipientNumber = "+971503920250"; // Replace with your WhatsApp number
     const whatsappURL = `https://wa.me/${recipientNumber}?text=${message}`;
 
     window.open(whatsappURL, '_blank');
